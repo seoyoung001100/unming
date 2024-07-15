4dc5ae6e8cbf5cd60d95afea93a9e98aad8e0aac
westzer001 <westzer0@amuz.co.kr>
2024-07-15 17:45:02 +0900

    class 수정 및 날씨 API 추가

--- src/App.vue
+++ src/App.vue
@@ -4,51 +4,51 @@
             <!-- 제일 상단의 구름 아이콘 -->
              <div class="pt-8 w-100% h-40">
                 <img id="cloud-icon" src="./assets/icons/cloud.png"/>
-                <h1 id="center" class="text-white text-2xl">Considerando</h1>  <!-- Considerando -->
+                <h1 class="center text-white text-2xl">Considerando</h1>  <!-- Considerando -->
              </div>
             
 
             <!-- 메인 카드? 들 -->
-             <div id="c-center-sb" class="w-[1000px] h-[500px] mx-auto mt-0.4">
-                 <span id="r-center-sb" class=" w-[100%] h-[90%]">
+             <div class="c-center-sb w-[1000px] h-[500px] mx-auto mt-0.4">
+                 <span class="r-center-sb w-[100%] h-[90%]">
                     <!-- 왼쪽의 이미지 구상 (세로로 길게 2개) -->
-                    <div id="r-center-se" class=" w-[22%] h-[100%]">
-                        <div id="center" class="bg-slate-100 w-[35%] h-[90%] -translate-y-6 translate-x-2 rounded-full "></div>
-                        <div id="center" class="bg-slate-100 w-[35%] h-[90%] translate-y-6 -translate-x-2 rounded-full "></div>
+                    <div class="r-center-se w-[22%] h-[100%]">
+                        <div class="center bg-slate-100 w-[35%] h-[90%] -translate-y-6 translate-x-2 rounded-full "></div>
+                        <div class="center bg-slate-100 w-[35%] h-[90%] translate-y-6 -translate-x-2 rounded-full "></div>
                     </div>
                     <!-- 중앙의 메인 카드 (가로로 2개가 들어 갈 예정)-->
-                    <div id="c-center-sb" class=" w-[56%] h-[100%]">
+                    <div class="c-center-sb w-[56%] h-[100%]">
                         <!-- main 이미지 -->
-                        <div id="center" class=" w-[100%] h-[80%]">
+                        <div class="center w-[100%] h-[80%]">
                             <div class="bg-slate-100 w-[95%] h-[85%] rounded-[10px]"></div>
                         </div>
                         <!-- 헤더 이미지 느낌 -->
-                        <div id="c-center-sb" class=" w-[100%] h-[20%]">
+                        <div class="c-center-sb w-[100%] h-[20%]">
                             <div class="bg-slate-100 w-[95%] h-[75%] rounded-[10px]"></div>
                         </div>
                     </div>
                     <!-- 오른쪽의 목록 구상 (뭘 넣을지는 딱히 못 정했음...)-->
-                    <div id="c-center-sb" class=" w-[22%] h-[100%]">
+                    <div class="w-[22%] h-[100%]">
                         <div class=" w-[100%] h-[70%]">
                             <!-- 오늘 날짜 -->
                             <div class="w-[100%] h-[15.3%]">
-                                <h1 id="center" class="text-white text-[20px] mb-0.5 -translate-y-1">{{ currentTime }}</h1>
+                                <h1 class="center text-white text-[20px] mb-0.5 -translate-y-1">{{ currentTime }}</h1>
                                 <hr class="mx-auto w-[85%] border-[0.5px]">
                             </div>
                             <!-- 기념일? 정보 -->
-                            <div id="r-end-sb" class="w-[100%] h-[85%]">
+                            <div class="r-end-sb w-[100%] h-[85%]">
                                 <div class="w-[90%] h-[100%]">
-                                    <div id="c-end-sb" class="text-white w-[100%] h-1/3" v-for="initdete in date" :key="initdete.value">
-                                        <span id="c-center" class="h-[100%]">
-                                            <h2 class="text-[16.7px]">{{ initdete.name }}</h2>
+                                    <div class="text-white w-[100%] h-1/3" v-for="initdete in date" :key="initdete.value">
+                                        <span class="c-end h-[100%]">
+                                            <p class="text-[16.7px]">{{ initdete.name }}</p>
                                             <p class="text-[11px]">{{ initdete.value }}</p> 
                                         </span>
                                         
                                     </div>
                                 </div>
                                 <div class="w-[10%] h-[100%]">
-                                    <div id="c-end-sb" class="w-[100%] h-1/3" v-for="index in 3" :key="index">
-                                        <span id="center" class="text-xl h-[100%] text-white">⟢</span>
+                                    <div class="c-end-sb w-[100%] h-1/3" v-for="index in 3" :key="index">
+                                        <span class="center text-xl h-[100%] text-white">⟢</span>
                                     </div>
                                 </div>
                                 
@@ -56,13 +56,25 @@
                         </div>
 
                         <!-- 날씨가 나오는 부분 -->
-                        <div class="bg-slate-500 w-[100%] h-[30%]"></div>
+                        <div class="r-center-se w-[100%] h-[30%]">
+                            <div class="center  w-[40%] h-[70%]">
+                                <img  
+                                    :src="weatherIconHref"
+                                    alt="Weather Icon" 
+                                    class="w-[70px]"
+                                    />
+                            </div>
+                            <div class="c-center w-[60%] h-[70%]">
+                                <h2 class="center text-white text-2xl">{{ weatherMain }}</h2>
+                                <p class="center text-white">{{ weatherDescription }}</p>
+                            </div>
+                        </div>
                     </div>
                  </span>
 
                  <!-- 제일 하단의 카테고리 -->
-                 <span id="r-center-se" class="w-[50%] h-[10%]" >
-                    <div v-for="categorys in category" :key="categorys.title" id="center" class="bg-slate-50 w-[95px] h-[29px] rounded-full">
+                 <span class="r-center-se w-[50%] h-[10%]" >
+                    <div v-for="categorys in category" :key="categorys.title" class="center bg-slate-50 w-[95px] h-[29px] rounded-full">
                         <p class="text-[11px]">{{ categorys.title }}</p>
                     </div>
                  </span>
@@ -78,22 +90,20 @@ import axios from 'axios';
 import dayjs from 'dayjs';
 import "dayjs/locale/ko";
 dayjs.locale("ko") //글로벌로 한국어를 사용한다
-import { onMounted } from 'vue'; 
+import { onMounted, ref } from 'vue'; 
 
-// const cityName = ref('');
-// const mainTemp = ref('');
-// const weatherMain = ref('');
-// const weatherDescription = ref('');
-// const weatherIcon = ref('');
-// const weatherIconHref = ref('');
+const weatherMain = ref('');
+const weatherDescription = ref('');
+const weatherIcon = ref('');
+const weatherIconHref = ref('');
 
 const currentTime = dayjs().format("YYYY. MM. DD."); //오늘 날짜 받아오기
 
 // 기념일? 데이터
 const date = [
     {
-        name: 'LEE SEOYOUNG',
-        value: '2004. 08. 26',
+        name: 'NONGDAMGOM',
+        value: '2014. 08. 01',
     },
     {
         name: 'KOO JUNGMO',
@@ -132,20 +142,13 @@ onMounted(() => {
             let initalResponseData = response.data;
             console.log('원하는 값: ',initalResponseData.weather[0].icon);
 
-            // cityName.value = initalResponseData.name;
-            // mainTemp.value = initalResponseData.main.temp;
-
-            // temporaryData[0].value = initalResponseData.main.humidity + '%';
-            // temporaryData[1].value = initalResponseData.wind.speed + 'm/s';
-            // temporaryData[2].value = Math.round(initalResponseData.main.feels_like) + "°";
-
-            // weatherMain.value = initalResponseData.weather[0].main;
-            // weatherDescription.value = initalResponseData.weather[0].description;
-            // weatherIcon.value = initalResponseData.weather[0].icon;
+            weatherMain.value = initalResponseData.weather[0].main;
+            weatherDescription.value = initalResponseData.weather[0].description;
+            weatherIcon.value = initalResponseData.weather[0].icon;
 
             // // assets 폴더가 components 폴더를 외부 폴더로 인식을 했기 때문에 경로를 제대로 인식하지 못했다
             // // require() 함수로 경로를 인코딩 후에 불러오면 잘 들어가진다
-            // weatherIconHref.value = require('../assets/images/'+ weatherIcon.value+'.png'); 
+            weatherIconHref.value = require('./assets/icons/'+ weatherIcon.value+'.png'); 
         })
         .catch(error => {console.log(error);})
 })
@@ -155,7 +158,9 @@ onMounted(() => {
 <style scoped>
     body {
         margin: 0; /* 기본 여백 제거 */
-        overflow: hidden; /* 스크롤 숨기기 */
+        overflow: hidden;
+        width: 100%;
+        position: fixed; 
     }
     h1{
         font-family: "Playwrite CU", cursive;
@@ -163,6 +168,19 @@ onMounted(() => {
         font-weight: 335;
         font-style: normal;
     }
+    h2{
+        font-family: "Playwrite PE", cursive;
+        font-optical-sizing: auto;
+        font-weight: 300;
+        font-style: normal;
+    }
+    p{
+        font-family: "Montserrat", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+    }
+
     #container {
         background-image:
          linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.814)),
@@ -181,24 +199,30 @@ onMounted(() => {
 
 
     /* ---------- 기본적인 큰 스타일 지정 세부적인 조절은 테일윈드를 사용한다. ---------- */
-    #center{
+    .center{
         display: flex;
         align-items: center;
         justify-content: center;
     }
-    #center-sb{
+    .center-sb{
         display: flex;
         align-items: center;
         justify-content: space-between;
     }
-    #center-se{
+    .center-se{
         display: flex;
         align-items: center;
         justify-content: space-evenly;
     }
-    #c-center{
+    .c-end{
         display: flex;
         align-items: end;
+        justify-content: center;
+        flex-direction: column;
+    }
+    .c-center{
+        display: flex;
+        align-items: center;
         justify-content: center;
         flex-direction: column;
     }
@@ -208,14 +232,14 @@ onMounted(() => {
         justify-content: flex-end;
         flex-direction: column;
     }
-    #c-end-sb{
+    .c-end-sb{
         display: flex;
         align-items: end;
         justify-content: center;
         justify-content: space-between;
         flex-direction: column;
     }
-    #c-center-sb{
+    .c-center-sb{
         display: flex;
         align-items: center;
         justify-content: space-between;
@@ -227,19 +251,19 @@ onMounted(() => {
         justify-content: space-evenly;
         flex-direction: column;
     }
-    #r-center-se{
+    .r-center-se{
         display: flex;
         align-items: center;
         justify-content: space-evenly;
         flex-direction: row;
     }
-    #r-center-sb{
+    .r-center-sb{
         display: flex;
         align-items: center;
         justify-content: space-between;
         flex-direction: row;
     }
-    #r-end-sb{
+    .r-end-sb{
         display: flex;
         align-items: end;
         justify-content: center;
