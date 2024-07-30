d50f8b0265c3ade01b6752917ad439fa4d4f24a2
westzer001 <westzer0@amuz.co.kr>
2024-07-30 17:56:55 +0900

    갤러리 뷰 수정

--- src/components/GalleryCard.vue
+++ src/components/GalleryCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link to="/profile/ProfileView" class="bg-imges-size w-[31%] h-[75%] bg-slate-400 rounded-3xl duration-500 hover:scale-[1.02] shadow-lg" v-for="item in props.date" :key="item.name" :style="{'background-image': `url(${item.img || ''})`}">
+    <router-link to="/profile/ProfileView" class="bg-imges-size w-[30%] h-[74%] bg-slate-400 rounded-3xl duration-500 hover:scale-[1.02] shadow-lg" v-for="item in props.date" :key="item.name" :style="{'background-image': `url(${item.img || ''})`}">
         <div class="w-[100%] h-[75%]"></div>
         <div class="c-center w-[100%] h-[25%] mx-auto bg-slate-50 rounded-b-3xl">
             <p class="text-[#233964] text-[28px]">{{ item.name }}</p>
